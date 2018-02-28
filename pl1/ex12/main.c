514c7ea5976ddd9731376b6c87a7af5ceffb3dd7
Raúl Correia <1090657@isep.ipp.pt>
2018-02-28 17:17:17 +0000

    ex12

---
+++ pl1/ex12/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int spawn_childs(int n);
+
+int main()
+{
+    const int NUM_PROC = 6; //NUM CHILDS
+    int num = spawn_childs(NUM_PROC); // criar NUM_CHILDS
+    if (num == 0) // Se estamos no processo pai
+    {
+        int i;
+        for (i = 0; i < NUM_PROC; ++i)//VAMOS ESPERAR NUM_PROC VEZES
+        {
+            wait(NULL);
+        }
+    }
+    printf("Num processo:%d -- %d*2 = %d\n", num, num, num * 2);//Escrever output
+    return 0;
+}
+
+int spawn_childs(int n)
+{
+    if (n < 0)
+    {
+        return -1;
+    }
+    int i;
+    for (i = 1; i <= n; ++i)
+    {
+        pid_t pid = fork();
+        if (pid == -1)
+        {
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            if (pid == 0)
+            {
+                break;
+            }
+        }
+    }
+    if (i > n)
+    {
+        i = 0;
+    }
+    return i;
+}
