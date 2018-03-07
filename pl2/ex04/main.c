fd94a10a441b59be8d5a2bff5f062d1032aad993
Raúl Correia <1090657@isep.ipp.pt>
2018-03-07 16:38:02 +0000

    ex04

--- pl2/ex04/main.c
+++ pl2/ex04/main.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 
-#define BUF_SIZE 2048
+#define BUF_SIZE 256
 int main()
 {
     char buf[BUF_SIZE];
@@ -16,17 +16,33 @@ int main()
     }
 
     pid_t pid = fork();
-    if(pid == -1 ){
+    if (pid == -1)
+    {
         return EXIT_FAILURE;
     }
-    else{
-        if(pid == 0){
+    else
+    {
+        if (pid == 0)
+        {
             close(fd[1]);
-            read(fd[0],(void*)buf,BUF_SIZE);
-        }else{
+            while (read(fd[0], (void *)buf, BUF_SIZE))
+            {
+                printf("%s", buf);
+            }
+            close(fd[0]);
+        }
+        else
+        {
+            close(fd[0]);
             FILE *txtfile;
-            txtfile = fopen("./text.txt","r+");
-
+            txtfile = fopen("./text.txt", "r+");
+            while (fgets(buf, BUF_SIZE, txtfile) != NULL)
+            {
+                write(fd[1], (void *)buf, strlen(buf));
+            }
+            int status;
+            close(fd[1]);
+            wait(&status);
         }
     }
 
