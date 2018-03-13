eda59c968ec0ec801e7b4e766f5ed374fb691a5b
Mario Dias <1151708@isep.ipp.pt>
2018-03-13 15:40:34 +0000

    Ex 05 resolvido

--- pl2/ex05/main.c
+++ pl2/ex05/main.c
@@ -4,15 +4,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MESSAGE_SIZE 256
 
-void convertString(char *str, int flag)
+void convertString(char *str)
 {
 
     while (*str != '\0')
     {
-
         if (islower(*str))
         {
             *str = toupper(*str);
@@ -33,12 +33,18 @@ int main()
     pid_t pid;
     int client[2];
     int server[2];
-    int status;
-    char message[MESSAGE_SIZE];
+    char messageClient[MESSAGE_SIZE]; // mensagem que vai ser feita o input de utilizador
+    char receivedMessageClient[MESSAGE_SIZE]; // mensagem que server vai receber de client
+    char receivedMessageServer[MESSAGE_SIZE]; // mensagem que client vai receber de server
     
     if (pipe(client) == -1) //teste para verificar sucesso pipe
     {
-        perror("FAIL PIPE");
+        perror("FAIL PIPE CLIENT");
+        return EXIT_FAILURE;
+    }
+    if (pipe(server) == -1) //teste para verificar sucesso pipe
+    {
+        perror("FAIL PIPE SERVER");
         return EXIT_FAILURE;
     }
     pid = fork();  //criação de um processo
@@ -49,23 +55,41 @@ int main()
     }
     if (pid > 0)
     {
-        wait(&status);
-        if (WIFEXITED(status))
-        {
-            close(client[1]);
-
+        close(client[1]); // fechar escrita de pipe para fazer leitura
+        if(read(client[0], (void *)receivedMessageClient,MESSAGE_SIZE ) == -1) { // verificação de sucesso na leitura do pipe client
+            perror("Erro na leitura de client Pai");
+            return EXIT_FAILURE;
         }
+        close(client[0]);// fechar pipe de leitura de client
+        printf("---Server---\nReceived message: %s\n",receivedMessageClient);
+        convertString(receivedMessageClient); // metodo que converte letras minusculas em Maiusculas e vice-versa        
+        
+        close(server[0]);// fechar pipe de leitura para escrita server
+        if(write(server[1], (void *)receivedMessageClient, MESSAGE_SIZE) == -1){ // escrita no pipe com teste de sucesso
+            perror ("Erro escrita pelo Server");
+            return EXIT_FAILURE;
+        }
+        close(server[1]); // fechar o pipe de escrita
     }
     if(pid == 0) //se processo filho
     {
-        close(client[0]);
-        scanf("%s",message);
-        if(write(client[1], (void *)message, MESSAGE_SIZE) == -1){
+        close(client[0]); // fechar pipe client de leitura para fazer escrita
+        printf("Insira a palavra para enviar para server: ");
+        scanf("%s",messageClient); //input de nova string para se escrever no pipe
+        if(write(client[1], (void *)messageClient, MESSAGE_SIZE) == -1){ // escrita no pipe com teste de sucesso
             perror ("Erro escrita pelo filho");
             return EXIT_FAILURE;
         }
-        close(client[1]);
-        exit(2);
+        close(client[1]); //fechar pipe de escrita apos a escrita no pipe client
+        
+        close(server[1]); // fechar pipe de escrita para faszer leitura pipe server
+        if(read(server[0], (void *)receivedMessageServer,MESSAGE_SIZE ) == -1) { // leitura de pipe com teste de sucesso para receber e imprimir a nova palavra convertida do server
+            perror("Erro na leitura de Client");
+            return EXIT_FAILURE;
+        }
+        close(server[0]); // fechar leitura de pipe apos leitura
+        printf("---Client---\nReceived and converted message: %s\n",receivedMessageServer);
+        exit(2);//terminar processo 
     }
     return 0;
 }
