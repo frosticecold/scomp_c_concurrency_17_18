9ca3607399825047dfa37a5a443a0fd4535e9a05
Raúl Correia <1090657@isep.ipp.pt>
2018-04-03 14:24:48 +0100

    inicio ex02

---
+++ pl3/ex02/main.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int main(){}
