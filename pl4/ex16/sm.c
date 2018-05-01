0f792aec73c210194af556f185004c3a1a30e815
Raúl Correia <1090657@isep.ipp.pt>
2018-05-01 22:18:30 +0100

    ex16

---
+++ pl4/ex16/sm.c
@@ -0,0 +1,46 @@
+#include "sm.h"
+shared_memory *create_shared_memory(int flags)
+{
+    int fd;
+    const char *const workingdir = SHM_FILENAME;
+    //int flags = O_CREAT | O_EXCL | O_RDWR;
+    mode_t mode = S_IRUSR | S_IWUSR;
+    fd = shm_open(workingdir, flags, mode);
+    if (fd == -1)
+    {
+        perror("Erro ao criar memoria partilhada.");
+        return NULL;
+    }
+    if (ftruncate(fd, sizeof(shared_memory)) == -1)
+    {
+        perror("Erro ao truncar a memória.");
+        return NULL;
+    }
+    int prot_flags = PROT_READ | PROT_WRITE;
+    shared_memory *addr = mmap(NULL, sizeof(shared_memory)
+    , prot_flags, MAP_SHARED, fd, 0);
+    if (addr == MAP_FAILED)
+    {
+        perror("Erro ao fazer map à memória.");
+        return NULL;
+    }
+    //memset(&addr[0], 0, sizeof(shared_memory));
+    addr->fd = fd;
+    return addr;
+}
+void delete_shared_memory(shared_memory *addr)
+{
+    int fd = addr->fd;
+    munmap(addr, sizeof(shared_memory));
+    close(fd);
+    shm_unlink(SHM_FILENAME);
+}
+void check_if_shm_exists()
+{
+
+    if (access(SHM_DELETE, F_OK) == 0)
+    {
+        if (unlink(SHM_DELETE) < 0)
+            perror("Erro ao apagar.");
+    }
+}
---
+++ pl4/ex16/sm.h
@@ -0,0 +1,25 @@
+#ifndef SHAREDMEMORY_H
+#define SHAREDMEMORY_H
+
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>  //perror
+#include <string.h> //memset
+#define SHM_FILENAME "/pl4_ex16"
+#define SHM_DELETE "/dev/shm" SHM_FILENAME
+#define SHM_DEFAULT_FLAGS O_CREAT | O_RDWR
+/** shared memory struct */
+typedef struct
+{
+    int fd;
+   int num_cars_east;
+   int num_cars_west;
+} shared_memory;
+
+shared_memory *create_shared_memory(int flags);
+void delete_shared_memory(shared_memory *addr);
+void check_if_shm_exists();
+#endif
